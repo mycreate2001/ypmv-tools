76c0a2661eb7561470ebf98afea8849f41ab1f2d
mycreate2001 <mycreate2001@gmail.com>
2022-02-28 14:27:10 +0700

    start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,11 @@ const routes: Routes = [
   {
     path: 'camera',
     loadChildren: () => import('./modals/camera/camera.module').then( m => m.CameraPageModule)
+  },  {
+    path: 'qrcode',
+    loadChildren: () => import('./modals/qrcode/qrcode.module').then( m => m.QrcodePageModule)
   }
+
 ];
 @NgModule({
   imports: [
