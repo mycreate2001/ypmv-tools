4d2ba73bf518df35fd8ac618d2cdfe30a4993714
mycreate2001 <mycreate2001@gmail.com>
2022-03-21 20:24:19 +0700

    unit\base64: add extension, revise case is not base64

--- src/app/utils/base64.ts
+++ src/app/utils/base64.ts
@@ -4,16 +4,20 @@ contentType:	"image/png"
 data:			"iVBORw0KGgoAAAANSUhEUg..."
 */
 export class Base64{
-    data:string;
-    contentType:string;
+    data:string='';
+    contentType:string='';
+    extension:string='jpg';
     constructor(base64:string){
+        this.data=base64;
         const str=base64.substring(0,50);
         const _1ST="data:";
         const _2ND=";base64,";
+        if(!base64.includes(_2ND)) {console.log("data is not base64");return }
         const first=_1ST.length
         const last=str.indexOf(_2ND);
         this.contentType=str.substring(first,last);
         this.data=base64.substring(last+_2ND.length);
-        console.log({data:this.data,contentType:this.contentType})
+        this.extension=this.contentType.split("/")[1];
+        console.log({data:str,contentType:this.contentType,extention:this.extension})
     }
 }
