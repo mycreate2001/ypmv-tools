7fe30d695a8a354ae140dda7601cccd7c77a4c0e
mycreate2001 <mycreate2001@gmail.com>
2022-04-06 15:14:50 +0700

    temporary

--- src/app/modals/profile/profile.page.ts
+++ src/app/modals/profile/profile.page.ts
@@ -1,23 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-import { UserData } from 'src/app/models/user.model';
+import { UserData, _DB_USERS, _STORAGE_USERS } from 'src/app/models/user.model';
+import { UrlData } from 'src/app/models/util.model';
 import { DisplayService } from 'src/app/services/display/display.service';
 import { FirestoreService } from 'src/app/services/firebase/firestore.service';
 import { StorageService } from 'src/app/services/firebase/storage.service';
 import { CameraPage } from '../camera/camera.page';
-const _DB_USER="users"
-const _STORAGE_AVATAR="avatars"
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-  /** variable */
-  user:UserData=null;
-  isChange:boolean=false;
-  isEdit:boolean=false;
-  image:string;
+  /** input */
+  userId:string;
+  user:UserData=null;           // user data
+
+  /** internal */
+  isChange:boolean=false;       // check change data
+  isOwner:boolean=false;        // Own acount
+  isAvailable:boolean=false;    // enable display data
+  viewImage:string;                 // new image
+  addImage:string;
+
   constructor(
     private modal:ModalController,
     private disp:DisplayService,
@@ -27,44 +32,49 @@ export class ProfilePage implements OnInit {
 
   }
 
+  ////////////// SYSTEM FUNCTIONS /////////////////
   ngOnInit() {
   }
 
-  done(role:string='OK'){
-    this.modal.dismiss({image:this.image,user:this.user},role)
+
+  ////////////// BUTTONS & EVENTS HANDLER ////////
+  /** Exit page */
+  done(role:ProfilePageRole='ok'){
+    const out:ProfilePageOuts={
+      user:this.user
+    }
+    this.modal.dismiss(out,role)
   }
 
-  addImage(){
-    this.disp.showModal(CameraPage,{ratio:1,fix:true})
-    .then(result=>{
-      if(result.role.toUpperCase()!='OK') return;
-      this.isChange=true;
-      console.log("image-size:",result.data.length);
-      this.image=result.data;
-    })
-  }
-
+  /** save profile */
   save(){
-    this.uploadImage()
-    .then(user=>{
-      return this.db.add(_DB_USER,user);
+    const image:UrlData={url:this.addImage,caption:''}
+    this.storage.uploadImages([image],_STORAGE_USERS)
+    .then((images:UrlData[])=>{
+      const image=images[0].url
+      this.user.image=image;
+      return this.db.add(_DB_USERS,this.user)
     })
-    .then(()=>{
-      this.done()
-    })
-    .catch(err=>this.disp.msgbox("error<br>"+err.message))
+    .then(()=>this.done())
   }
 
-  uploadImage():Promise<UserData>{
-    return new Promise((resolve,reject)=>{
-      if(!this.image) return resolve(this.user);
-      this.storage.uploadImagebase64(this.image,`${_STORAGE_AVATAR}/${this.user.id}.jpg`)
-      .then(u=>{
-        this.user.image=u.url;
-        return resolve(this.user)
-      })
-      .catch(err=>reject(err))
-    })
+  ///////////// BACKGROUND FUNCTIONS //////////////
+  update(){
+    this.viewImage=this.addImage||this.user.image
   }
 
 }
+
+///////////////// INTERFACE //////////////////////////////
+export interface ProfilePageOpts{
+  userId?:string;
+  user:UserData;
+}
+
+export interface ProfilePageOuts{
+  user:UserData;
+}
+
+export type ProfilePageRole="ok"|"back"
+
+
